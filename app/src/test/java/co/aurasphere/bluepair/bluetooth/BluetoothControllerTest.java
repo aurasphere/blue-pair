012ef3e57613c051031100005a110baf922afb28
Yegor Voronyansky <vrnsky@protonmail.ch>
2017-04-23 13:41:19 +0400

    add new test cases for bt controller

--- app/src/test/java/co/aurasphere/bluepair/bluetooth/BluetoothControllerTest.java
+++ app/src/test/java/co/aurasphere/bluepair/bluetooth/BluetoothControllerTest.java
@@ -3,19 +3,21 @@ package co.aurasphere.bluepair.bluetooth;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.widget.Toast;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-
+import java.util.HashSet;
+import java.util.Set;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for Bluetooth Controller.
@@ -56,6 +58,110 @@ public class BluetoothControllerTest {
         when(BluetoothAdapter.getDefaultAdapter()).thenReturn(adapter);
         BluetoothController controller = new BluetoothController(activity, adapter, listener);
         controller.turnOnBluetooth();
+        verify(adapter).enable();
+    }
+
+    /**
+     * When try call discovering method on bluetooth should check that is called.
+     * @throws Exception if something error happened.
+     */
+    @Test
+    public void whenTryCallDiscoveringMethodOnBluetoothShouldCheckThatIsCalled() throws Exception {
+        Activity activity = mock(Activity.class);
+        BluetoothDiscoveryDeviceListener listener = mock(BluetoothDiscoveryDeviceListener.class);
+        BluetoothAdapter adapter = mock(BluetoothAdapter.class);
+        mockStatic(BluetoothAdapter.class);
+        when(BluetoothAdapter.getDefaultAdapter()).thenReturn(adapter);
+        BluetoothController controller = new BluetoothController(activity, adapter, listener);
+        controller.isDiscovering();
+        verify(adapter).isDiscovering();
+    }
+
+
+    /**
+     * When try get pair status of null device should check that method throw exception.
+     * @throws Exception if some error happened.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void whenTryBoundingDeviceButDeviceIsNullShouldCheckThatThrowException() throws Exception {
+        Activity activity = mock(Activity.class);
+        BluetoothDiscoveryDeviceListener listener = mock(BluetoothDiscoveryDeviceListener.class);
+        BluetoothAdapter adapter = mock(BluetoothAdapter.class);
+        mockStatic(BluetoothAdapter.class);
+        when(BluetoothAdapter.getDefaultAdapter()).thenReturn(adapter);
+        BluetoothController controller = new BluetoothController(activity, adapter, listener);
+        controller.getPairingDeviceStatus();
+    }
+
+    /**
+     * When try get device name should check that all is ok.
+     * @throws Exception if some error happened.
+     */
+    @Test
+    public void whenTryGetDeviceNameShouldCheckThatAllIsOK() throws Exception {
+        BluetoothDevice device = mock(BluetoothDevice.class);
+        when(device.getName()).thenReturn("android");
+        assertThat(BluetoothController.getDeviceName(device), is("android"));
+    }
+
+    /**
+     * When try get device name but it is null should check that controller return address.
+     * @throws Exception if some error happened.
+     */
+    @Test
+    public void whenTryGetDeviceNameButItIsNullShouldCheckThatControllerReturnAddress() throws Exception {
+        BluetoothDevice device = mock(BluetoothDevice.class);
+        when(device.getName()).thenReturn(null);
+        when(device.getName()).thenReturn("08-ED-B9-49-B2-E5");
+        assertThat(BluetoothController.getDeviceName(device), is("08-ED-B9-49-B2-E5"));
+    }
+
+    /**
+     * When try check that devices already paired should check that method works correct.
+     * @throws Exception if some error happened.
+     */
+    @Test
+    public void whenTryCheckThatDevicesAlreadyPairedShouldCheckThatMethodWorksCorrect() throws Exception {
+        Activity activity = mock(Activity.class);
+        Set<BluetoothDevice> devices = new HashSet<>();
+        BluetoothDevice device = mock(BluetoothDevice.class);
+        devices.add(device);
+        BluetoothDiscoveryDeviceListener listener = mock(BluetoothDiscoveryDeviceListener.class);
+        BluetoothAdapter adapter = mock(BluetoothAdapter.class);
+        mockStatic(BluetoothAdapter.class);
+        when(BluetoothAdapter.getDefaultAdapter()).thenReturn(adapter);
+        when(adapter.getBondedDevices()).thenReturn(devices);
+        BluetoothController controller = new BluetoothController(activity, adapter, listener);
+        assertThat(controller.isAlreadyPaired(device), is(true));
+    }
+
+
+    /**
+     * When try cast device to string should check that all is ok.
+     * @throws Exception if some error happened.
+     */
+    @Test
+    public void whenTryCastDeviceToStringShouldCheckThatAllIsOk() throws Exception {
+        BluetoothDevice device = mock(BluetoothDevice.class);
+        when(device.getName()).thenReturn("device");
+        when(device.getAddress()).thenReturn("08-ED-B9-49-B2-E5");
+        String actual = "[Address: 08-ED-B9-49-B2-E5, Name: device]";
+        assertEquals(actual, BluetoothController.deviceToString(device));
+    }
+
+    /**
+     * When turn on bluetooth and schedule discovery should check that bluetooth enabled.
+     * @throws Exception if some error happeened.
+     */
+    @Test
+    public void whenTurnOnBluetoothAndScheduleDiscoveryShouldCheckThatBTenabled() throws Exception {
+        Activity activity = mock(Activity.class);
+        BluetoothDiscoveryDeviceListener listener = mock(BluetoothDiscoveryDeviceListener.class);
+        BluetoothAdapter adapter = mock(BluetoothAdapter.class);
+        mockStatic(BluetoothAdapter.class);
+        when(BluetoothAdapter.getDefaultAdapter()).thenReturn(adapter);
+        BluetoothController controller = new BluetoothController(activity, adapter, listener);
+        controller.turnOnBluetoothAndScheduleDiscovery();
         verify(adapter).enable();
     }
 }
